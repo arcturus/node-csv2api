63b7f56b41000c2f4228fe4d6f428353df07b56b
Francisco Jordano <arcturus@ardeenelinfierno.com>
2014-01-07 20:28:25 +0100

    Latest adjustements to the csv 2 leveldb importer

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,6 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    complexity: {
-      generic: {
-        src: ['index.js', 'lib/*.js'],
-        options: {
-          errorsOnly: false,
-          cyclometric: 6,       // default is 3
-          halstead: 16,         // default is 8
-          maintainability: 100  // default is 100
-        }
-      }
-    },
     jshint: {
       all: [
         'Gruntfile.js',
@@ -44,12 +33,11 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-complexity');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-cli');
 
-  grunt.registerTask('test', ['complexity', 'jshint', 'mochacli', 'watch']);
-  grunt.registerTask('ci', ['complexity', 'jshint', 'mochacli']);
+  grunt.registerTask('test', ['jshint', 'mochacli', 'watch']);
+  grunt.registerTask('ci', ['jshint', 'mochacli']);
   grunt.registerTask('default', ['test']);
 };
