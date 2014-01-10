d3643353a70c1d4d6b4825cc9a2d0f403c9158c9
Francisco Jordano <arcturus@ardeenelinfierno.com>
2014-01-10 20:17:30 +0100

    express wrapper to the leveldb database taking into account how we generate the keys in csv2leveldb

--- index.js
+++ index.js
@@ -5,6 +5,92 @@
 // to allow search by the columns of the csv document.
 // The storage is supported via levelDB
 
-module.exports = function () {
-  return 'Hello, world';
-};
+var express = require('express'),
+    levelup = require('levelup');
+
+var csv2api = (function csv2api() {
+
+  var config, db;
+
+  var create = function create(options, csvPath) {
+
+  };
+
+  var init = function init(cols, dbPath, port) {
+    if (!cols || !dbPath) {
+      throw new Error('Insufficient parameters, please check documentation');
+    }
+
+    config = {
+      'port': port || 3000,
+      'dbPath': dbPath,
+      'cols': cols
+    };
+
+    initLevelDB();
+    serve();
+  };
+
+  var serve = function serve() {
+    var app = express();
+
+    app.get('/', handler);
+    app.get('/version', version);
+
+    app.listen(config.port);
+  };
+
+  var handler = function handler(req, res) {
+    var key = buildKey(req);
+
+    if (key === null) {
+      res.send(404, 'Invalid parameters, try with ' +
+        JSON.stringify(config.cols));
+      return;
+    }
+
+    queryLevelDB(key, res);
+  };
+
+  var buildKey = function buildKey(req) {
+    var key = [];
+
+    config.cols.forEach(function onCol(col) {
+      if (req.query[col]) {
+        key.push(col + '_' + req.query[col]);
+      }
+    });
+
+    return key.length === 0 ? null : key.join('_');
+  };
+
+  var queryLevelDB = function queryLevelDB(key, res) {
+    db.get(key, function onResponse(err, value) {
+      if (err) {
+        res.send(404, err);
+      } else {
+        res.send(200, value);
+      }
+    });
+  };
+
+  var initLevelDB = function initLevelDB() {
+    if (!config.dbPath) {
+      throw new Error('Unkonwn leveldb path');
+    }
+    db = levelup(config.dbPath, {keyEncoding: 'json', valueEncoding: 'json'});
+  };
+
+  var version = function version(req, res) {
+    var pkg = require('./package.json');
+    res.send(200, 'Version: ' + pkg.version);
+  };
+
+  return {
+    'init': init,
+    'create': create
+  };
+
+})();
+
+module.exports = csv2api;
--- test/index.js
+++ test/index.js
@@ -2,10 +2,3 @@
 
 var expect = require('expect.js'),
     nodeCsv2api = require('..');
-
-describe('node-csv2api', function () {
-  it('should say hello', function (done) {
-    expect(nodeCsv2api()).to.equal('Hello, world');
-    done();
-  });
-});
